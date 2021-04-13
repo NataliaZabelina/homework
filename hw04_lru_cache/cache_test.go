b7ffd0225d84041b55ae18adf41cfa966c76239d
natalya.zabelina <natalya.zabelina@iqoption.com>
2021-04-14 00:05:39 +0300

    tests refactoring
    tests refactoring

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -1,201 +1,187 @@
 package hw04lrucache
 
 import (
-	"math/rand"
-	"strconv"
-	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestCache(t *testing.T) {
-	t.Run("check get from empty cache", func(t *testing.T) {
-		c := NewCache(10)
-
-		_, ok := c.Get("aaa")
-		require.False(t, ok)
-
-		_, ok = c.Get("bbb")
-		require.False(t, ok)
-	})
-
-	t.Run("check Set and Get", func(t *testing.T) {
-		c := NewCache(5)
-
-		wasInCache := c.Set("aaa", 100)
-		require.False(t, wasInCache)
-
-		wasInCache = c.Set("bbb", 200)
-		require.False(t, wasInCache)
-
-		val, ok := c.Get("aaa")
-		require.True(t, ok)
-		require.Equal(t, 100, val)
-
-		val, ok = c.Get("bbb")
-		require.True(t, ok)
-		require.Equal(t, 200, val)
-
-		wasInCache = c.Set("aaa", 300)
-		require.True(t, wasInCache)
-
-		val, ok = c.Get("aaa")
-		require.True(t, ok)
-		require.Equal(t, 300, val)
-
-		val, ok = c.Get("ccc")
-		require.False(t, ok)
-		require.Nil(t, val)
-	})
-
-	t.Run("check capacity", func(t *testing.T) {
-		c := NewCache(3)
-		c.Set("item_1", 78)
-		c.Set("item_2", 73)
-		c.Set("item_3", 12)
-
-		val, ok := c.Get("item_1")
-		require.Equal(t, 78, val)
-		require.True(t, ok)
-
-		val, ok = c.Get("item_2")
-		require.Equal(t, 73, val)
-		require.True(t, ok)
-
-		val, ok = c.Get("item_3")
-		require.Equal(t, 12, val)
-		require.True(t, ok)
-
-		c.Set("item_4", 120)
-		val, ok = c.Get("item_4")
-		require.Equal(t, 120, val)
-		require.True(t, ok)
-
-		val, ok = c.Get("item_1")
-		require.False(t, ok)
-		require.Nil(t, val)
-	})
-
-	t.Run("remove items due to capacity", func(t *testing.T) {
-		c := NewCache(2)
-		c.Set("item_1", "ONE")
-		c.Set("item_2", "TWO")
-
-		val, ok := c.Get("item_1")
-		require.Equal(t, "ONE", val)
-		require.True(t, ok)
-
-		val, ok = c.Get("item_2")
-		require.Equal(t, "TWO", val)
-		require.True(t, ok)
-
-		c.Set("item_3", "THREE")
-		val, ok = c.Get("item_3")
-		require.Equal(t, "THREE", val)
-		require.True(t, ok)
-
-		val, ok = c.Get("item_1")
-		require.False(t, ok)
-		require.Nil(t, val)
-
-		c.Set("item_4", "FOUR")
-		val, ok = c.Get("item_4")
-		require.Equal(t, "FOUR", val)
-		require.True(t, ok)
-
-		val, ok = c.Get("item_2")
-		require.False(t, ok)
-		require.Nil(t, val)
-	})
-
-	t.Run("clear cache", func(t *testing.T) {
-		c := NewCache(2)
-		c.Set("item_1", true)
-		c.Set("item_2", false)
-
-		val, ok := c.Get("item_1")
-		require.Equal(t, true, val)
-		require.True(t, ok)
-
-		val, ok = c.Get("item_2")
-		require.Equal(t, false, val)
-		require.True(t, ok)
-
-		c.Clear()
-
-		val, ok = c.Get("item_1")
-		require.False(t, ok)
-		require.Nil(t, val)
-
-		val, ok = c.Get("item_2")
-		require.False(t, ok)
-		require.Nil(t, val)
-	})
-
-	t.Run("check lru logic", func(t *testing.T) {
-		c := NewCache(3)
-		c.Set("item_1", 6)
-		c.Set("item_2", 8)
-		c.Set("item_3", 2)
-
-		val, ok := c.Get("item_1")
-		require.Equal(t, 6, val)
-		require.True(t, ok)
-
-		val, ok = c.Get("item_2")
-		require.Equal(t, 8, val)
-		require.True(t, ok)
-
-		val, ok = c.Get("item_3")
-		require.Equal(t, 2, val)
-		require.True(t, ok)
-
-		c.Set("item_2", 90)
-		_, ok = c.Get("item_3")
-		require.True(t, ok)
-		c.Set("item_2", 88)
-		c.Set("item_3", 22)
-
-		c.Set("item_4", "FOUR")
-		val, ok = c.Get("item_4")
-		require.Equal(t, "FOUR", val)
-		require.True(t, ok)
-
-		val, ok = c.Get("item_1")
-		require.False(t, ok)
-		require.Nil(t, val)
-
-		val, ok = c.Get("item_2")
-		require.Equal(t, 88, val)
-		require.True(t, ok)
-
-		val, ok = c.Get("item_3")
-		require.Equal(t, 22, val)
-		require.True(t, ok)
-	})
+type CacheSuite struct {
+	suite.Suite
 }
 
-func TestCacheMultithreading(t *testing.T) {
-	t.Skip() // Remove me if task with asterisk completed.
-
+func (s *CacheSuite) TestGetFromEmptyCache() {
+	t := s.T()
 	c := NewCache(10)
-	wg := &sync.WaitGroup{}
-	wg.Add(2)
 
-	go func() {
-		defer wg.Done()
-		for i := 0; i < 1_000_000; i++ {
-			c.Set(Key(strconv.Itoa(i)), i)
-		}
-	}()
+	_, ok := c.Get("aaa")
+	require.False(t, ok)
 
-	go func() {
-		defer wg.Done()
-		for i := 0; i < 1_000_000; i++ {
-			c.Get(Key(strconv.Itoa(rand.Intn(1_000_000))))
-		}
-	}()
+	_, ok = c.Get("bbb")
+	require.False(t, ok)
+}
 
-	wg.Wait()
+func (s *CacheSuite) TestGetAndSetCheckCache() {
+	t := s.T()
+	c := NewCache(5)
+
+	wasInCache := c.Set("aaa", 100)
+	require.False(t, wasInCache)
+
+	wasInCache = c.Set("bbb", 200)
+	require.False(t, wasInCache)
+
+	val, ok := c.Get("aaa")
+	require.True(t, ok)
+	require.Equal(t, 100, val)
+
+	val, ok = c.Get("bbb")
+	require.True(t, ok)
+	require.Equal(t, 200, val)
+
+	wasInCache = c.Set("aaa", 300)
+	require.True(t, wasInCache)
+
+	val, ok = c.Get("aaa")
+	require.True(t, ok)
+	require.Equal(t, 300, val)
+
+	val, ok = c.Get("ccc")
+	require.False(t, ok)
+	require.Nil(t, val)
+}
+
+func (s *CacheSuite) TestCheckCapacityCache() {
+	t := s.T()
+	c := NewCache(3)
+	c.Set("item_1", 78)
+	c.Set("item_2", 73)
+	c.Set("item_3", 12)
+
+	val, ok := c.Get("item_1")
+	require.Equal(t, 78, val)
+	require.True(t, ok)
+
+	val, ok = c.Get("item_2")
+	require.Equal(t, 73, val)
+	require.True(t, ok)
+
+	val, ok = c.Get("item_3")
+	require.Equal(t, 12, val)
+	require.True(t, ok)
+
+	c.Set("item_4", 120)
+	val, ok = c.Get("item_4")
+	require.Equal(t, 120, val)
+	require.True(t, ok)
+
+	val, ok = c.Get("item_1")
+	require.False(t, ok)
+	require.Nil(t, val)
+}
+
+func (s *CacheSuite) TestRemoveItemsDueToCapacityCache() {
+	t := s.T()
+	c := NewCache(2)
+	c.Set("item_1", "ONE")
+	c.Set("item_2", "TWO")
+
+	val, ok := c.Get("item_1")
+	require.Equal(t, "ONE", val)
+	require.True(t, ok)
+
+	val, ok = c.Get("item_2")
+	require.Equal(t, "TWO", val)
+	require.True(t, ok)
+
+	c.Set("item_3", "THREE")
+	val, ok = c.Get("item_3")
+	require.Equal(t, "THREE", val)
+	require.True(t, ok)
+
+	val, ok = c.Get("item_1")
+	require.False(t, ok)
+	require.Nil(t, val)
+
+	c.Set("item_4", "FOUR")
+	val, ok = c.Get("item_4")
+	require.Equal(t, "FOUR", val)
+	require.True(t, ok)
+
+	val, ok = c.Get("item_2")
+	require.False(t, ok)
+	require.Nil(t, val)
+}
+
+func (s *CacheSuite) TestClearCache() {
+	t := s.T()
+	c := NewCache(2)
+	c.Set("item_1", true)
+	c.Set("item_2", false)
+
+	val, ok := c.Get("item_1")
+	require.Equal(t, true, val)
+	require.True(t, ok)
+
+	val, ok = c.Get("item_2")
+	require.Equal(t, false, val)
+	require.True(t, ok)
+
+	c.Clear()
+
+	val, ok = c.Get("item_1")
+	require.False(t, ok)
+	require.Nil(t, val)
+
+	val, ok = c.Get("item_2")
+	require.False(t, ok)
+	require.Nil(t, val)
+}
+
+func (s *CacheSuite) TestCheckLruLogic() {
+	t := s.T()
+	c := NewCache(3)
+	c.Set("item_1", 6)
+	c.Set("item_2", 8)
+	c.Set("item_3", 2)
+
+	val, ok := c.Get("item_1")
+	require.Equal(t, 6, val)
+	require.True(t, ok)
+
+	val, ok = c.Get("item_2")
+	require.Equal(t, 8, val)
+	require.True(t, ok)
+
+	val, ok = c.Get("item_3")
+	require.Equal(t, 2, val)
+	require.True(t, ok)
+
+	c.Set("item_2", 90)
+	_, ok = c.Get("item_3")
+	require.True(t, ok)
+	c.Set("item_2", 88)
+	c.Set("item_3", 22)
+
+	c.Set("item_4", "FOUR")
+	val, ok = c.Get("item_4")
+	require.Equal(t, "FOUR", val)
+	require.True(t, ok)
+
+	val, ok = c.Get("item_1")
+	require.False(t, ok)
+	require.Nil(t, val)
+
+	val, ok = c.Get("item_2")
+	require.Equal(t, 88, val)
+	require.True(t, ok)
+
+	val, ok = c.Get("item_3")
+	require.Equal(t, 22, val)
+	require.True(t, ok)
+}
+
+func TestCacheSuite(t *testing.T) {
+	suite.Run(t, new(CacheSuite))
 }
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -4,188 +4,208 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestList(t *testing.T) {
-	t.Run("empty list", func(t *testing.T) {
-		l := NewList()
+type ListSuite struct {
+	suite.Suite
+}
 
-		require.Equal(t, 0, l.Len())
-		require.Nil(t, l.Front())
-		require.Nil(t, l.Back())
-	})
+func (s *ListSuite) TestEmptyList() {
+	t := s.T()
+	l := NewList()
 
-	t.Run("check PushFront: push item to empty list", func(t *testing.T) {
-		l := NewList()
+	require.Equal(t, 0, l.Len())
+	require.Nil(t, l.Front())
+	require.Nil(t, l.Back())
+}
 
-		l.PushFront("Red")
-		require.Equal(t, 1, l.Len())
-		require.Equal(t, "Red", l.Front().Value)
-		require.Equal(t, "Red", l.Back().Value)
-	})
+func (s *ListSuite) TestPushFrontItemToEmptyList() {
+	t := s.T()
+	l := NewList()
 
-	t.Run("check PushFront: push items to list", func(t *testing.T) {
-		l := NewList()
+	l.PushFront("Red")
+	require.Equal(t, 1, l.Len())
+	require.Equal(t, "Red", l.Front().Value)
+	require.Equal(t, "Red", l.Back().Value)
+}
 
-		l.PushFront("Henry")
-		l.PushFront(98)
-		l.PushFront(true)
-		require.Equal(t, 3, l.Len())
-		require.Equal(t, true, l.Front().Value)
-		require.Equal(t, "Henry", l.Back().Value)
-	})
+func (s *ListSuite) TestPushFrontItemsToList() {
+	t := s.T()
+	l := NewList()
 
-	t.Run("check PushBack: push item to empty list", func(t *testing.T) {
-		l := NewList()
+	l.PushFront("Henry")
+	l.PushFront(98)
+	l.PushFront(true)
+	require.Equal(t, 3, l.Len())
+	require.Equal(t, true, l.Front().Value)
+	require.Equal(t, "Henry", l.Back().Value)
+}
 
-		l.PushBack(64)
-		require.Equal(t, 1, l.Len())
-		require.Equal(t, 64, l.Front().Value)
-		require.Equal(t, 64, l.Back().Value)
-	})
+func (s *ListSuite) TestPushBackItemToEmptyList() {
+	t := s.T()
+	l := NewList()
 
-	t.Run("check PushBack: push items to lists", func(t *testing.T) {
-		l := NewList()
+	l.PushBack(64)
+	require.Equal(t, 1, l.Len())
+	require.Equal(t, 64, l.Front().Value)
+	require.Equal(t, 64, l.Back().Value)
+}
 
-		l.PushBack(4)
-		l.PushBack(34)
-		l.PushBack("4")
-		l.PushBack(51)
-		require.Equal(t, 4, l.Len())
-		require.Equal(t, 4, l.Front().Value)
-		require.Equal(t, 51, l.Back().Value)
-	})
+func (s *ListSuite) TestRemoveFrontItemFromList() {
+	t := s.T()
+	l := NewList()
 
-	t.Run("check Remove front item", func(t *testing.T) {
-		l := NewList()
+	l.PushFront(6)
+	l.PushBack(18.9)
+	l.PushBack(92)
+	require.Equal(t, 3, l.Len())
 
-		l.PushFront(6)
-		l.PushBack(18.9)
-		l.PushBack(92)
-		require.Equal(t, 3, l.Len())
+	front := l.Front()
+	l.Remove(front)
 
-		front := l.Front()
-		l.Remove(front)
+	require.Equal(t, 2, l.Len())
+	require.Equal(t, 18.9, l.Front().Value)
+	require.Equal(t, 92, l.Back().Value)
+}
 
-		require.Equal(t, 2, l.Len())
-		require.Equal(t, 18.9, l.Front().Value)
-		require.Equal(t, 92, l.Back().Value)
-	})
+func (s *ListSuite) TestPushBackItemsToList() {
+	t := s.T()
+	l := NewList()
 
-	t.Run("check Remove middle item", func(t *testing.T) {
-		l := NewList()
+	l.PushBack(4)
+	l.PushBack(34)
+	l.PushBack("4")
+	l.PushBack(51)
+	require.Equal(t, 4, l.Len())
+	require.Equal(t, 4, l.Front().Value)
+	require.Equal(t, 51, l.Back().Value)
+}
 
-		l.PushFront(6)
-		l.PushBack(18)
-		l.PushBack(false)
-		require.Equal(t, 3, l.Len())
+func (s *ListSuite) TestRemoveMiddleItemFromList() {
+	t := s.T()
+	l := NewList()
 
-		middle := l.Front().Next
-		l.Remove(middle)
+	l.PushFront(6)
+	l.PushBack(18)
+	l.PushBack(false)
+	require.Equal(t, 3, l.Len())
 
-		require.Equal(t, 2, l.Len())
-		require.Equal(t, 6, l.Front().Value)
-		require.Equal(t, false, l.Back().Value)
-	})
+	middle := l.Front().Next
+	l.Remove(middle)
 
-	t.Run("check Remove back item", func(t *testing.T) {
-		l := NewList()
+	require.Equal(t, 2, l.Len())
+	require.Equal(t, 6, l.Front().Value)
+	require.Equal(t, false, l.Back().Value)
+}
 
-		l.PushFront(6.02)
-		l.PushBack(18)
-		l.PushBack(92.65)
-		require.Equal(t, 3, l.Len())
+func (s *ListSuite) TestRemoveBackItemFromList() {
+	t := s.T()
+	l := NewList()
 
-		back := l.Back()
-		l.Remove(back)
+	l.PushFront(6.02)
+	l.PushBack(18)
+	l.PushBack(92.65)
+	require.Equal(t, 3, l.Len())
 
-		require.Equal(t, 2, l.Len())
-		require.Equal(t, 6.02, l.Front().Value)
-		require.Equal(t, 18, l.Back().Value)
-	})
+	back := l.Back()
+	l.Remove(back)
 
-	t.Run("check Remove item from one element list", func(t *testing.T) {
-		l := NewList()
+	require.Equal(t, 2, l.Len())
+	require.Equal(t, 6.02, l.Front().Value)
+	require.Equal(t, 18, l.Back().Value)
+}
 
-		l.PushFront(70209)
-		require.Equal(t, 1, l.Len())
+func (s *ListSuite) TestRemoveFromOneItemList() {
+	t := s.T()
+	l := NewList()
 
-		item := l.Front()
-		l.Remove(item)
+	l.PushFront(70209)
+	require.Equal(t, 1, l.Len())
 
-		require.Equal(t, 0, l.Len())
-		require.Nil(t, l.Front())
-		require.Nil(t, l.Back())
-	})
+	item := l.Front()
+	l.Remove(item)
 
-	t.Run("check MoveToFront: one item list", func(t *testing.T) {
-		l := NewList()
-		l.PushFront(69)
+	require.Equal(t, 0, l.Len())
+	require.Nil(t, l.Front())
+	require.Nil(t, l.Back())
+}
 
-		l.MoveToFront(l.Front())
+func (s *ListSuite) TestMoveToFrontOneItemList() {
+	t := s.T()
+	l := NewList()
+	l.PushFront(69)
 
-		require.Equal(t, 1, l.Len())
-		require.Equal(t, 69, l.Front().Value)
-		require.Equal(t, 69, l.Back().Value)
-	})
+	l.MoveToFront(l.Front())
 
-	t.Run("check MoveToFront: two items in list", func(t *testing.T) {
-		l := NewList()
-		l.PushFront(69)
-		l.PushBack("one")
+	require.Equal(t, 1, l.Len())
+	require.Equal(t, 69, l.Front().Value)
+	require.Equal(t, 69, l.Back().Value)
+}
 
-		l.MoveToFront(l.Back())
+func (s *ListSuite) TestMoveToFrontTwoItemsList() {
+	t := s.T()
+	l := NewList()
+	l.PushFront(69)
+	l.PushBack("one")
 
-		require.Equal(t, 2, l.Len())
-		require.Equal(t, "one", l.Front().Value)
-		require.Equal(t, 69, l.Back().Value)
-	})
+	l.MoveToFront(l.Back())
 
-	t.Run("check MoveToFront: many items in list", func(t *testing.T) {
-		l := NewList()
-		l.PushFront(69)
-		l.PushBack(265)
-		l.PushFront(34)
-		l.PushBack(2) //34 69 265 2
+	require.Equal(t, 2, l.Len())
+	require.Equal(t, "one", l.Front().Value)
+	require.Equal(t, 69, l.Back().Value)
+}
 
-		l.MoveToFront(l.Back().Prev) // 265
+func (s *ListSuite) TestMoveToFrontManyItemsList() {
+	t := s.T()
+	l := NewList()
+	l.PushFront(69)
+	l.PushBack(265)
+	l.PushFront(34)
+	l.PushBack(2) // 34 69 265 2
 
-		require.Equal(t, 4, l.Len())
-		require.Equal(t, 265, l.Front().Value)
-		require.Equal(t, 2, l.Back().Value)
-	})
+	l.MoveToFront(l.Back().Prev) // 265
 
-	t.Run("multiple operations", func(t *testing.T) {
-		l := NewList()
+	require.Equal(t, 4, l.Len())
+	require.Equal(t, 265, l.Front().Value)
+	require.Equal(t, 2, l.Back().Value)
+}
 
-		l.PushFront(10) // [10]
-		l.PushBack(20)  // [10, 20]
-		l.PushBack(30)  // [10, 20, 30]
-		require.Equal(t, 3, l.Len())
+func (s *ListSuite) TestMultipleOperationsWithList() {
+	t := s.T()
+	l := NewList()
 
-		middle := l.Front().Next // 20
-		l.Remove(middle)         // [10, 30]
-		require.Equal(t, 2, l.Len())
+	l.PushFront(10) // [10]
+	l.PushBack(20)  // [10, 20]
+	l.PushBack(30)  // [10, 20, 30]
+	require.Equal(t, 3, l.Len())
 
-		for i, v := range [...]int{40, 50, 60, 70, 80} {
-			if i%2 == 0 {
-				l.PushFront(v)
-			} else {
-				l.PushBack(v)
-			}
-		} // [80, 60, 40, 10, 30, 50, 70]
+	middle := l.Front().Next // 20
+	l.Remove(middle)         // [10, 30]
+	require.Equal(t, 2, l.Len())
 
-		require.Equal(t, 7, l.Len())
-		require.Equal(t, 80, l.Front().Value)
-		require.Equal(t, 70, l.Back().Value)
-
-		l.MoveToFront(l.Front()) // [80, 60, 40, 10, 30, 50, 70]
-		l.MoveToFront(l.Back())  // [70, 80, 60, 40, 10, 30, 50]
-
-		elems := make([]int, 0, l.Len())
-		for i := l.Front(); i != nil; i = i.Next {
-			elems = append(elems, i.Value.(int))
+	for i, v := range [...]int{40, 50, 60, 70, 80} {
+		if i%2 == 0 {
+			l.PushFront(v)
+		} else {
+			l.PushBack(v)
 		}
-		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
-	})
+	} // [80, 60, 40, 10, 30, 50, 70]
+
+	require.Equal(t, 7, l.Len())
+	require.Equal(t, 80, l.Front().Value)
+	require.Equal(t, 70, l.Back().Value)
+
+	l.MoveToFront(l.Front()) // [80, 60, 40, 10, 30, 50, 70]
+	l.MoveToFront(l.Back())  // [70, 80, 60, 40, 10, 30, 50]
+
+	elems := make([]int, 0, l.Len())
+	for i := l.Front(); i != nil; i = i.Next {
+		elems = append(elems, i.Value.(int))
+	}
+	require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
+}
+
+func TestListSuite(t *testing.T) {
+	suite.Run(t, new(ListSuite))
 }
